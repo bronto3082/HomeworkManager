0a7704a18016e3d04abe39b5b896d0760367fae1
Hyunsoo Jang <bronto3082@gmail.com>
2021-11-11 16:40:05 +0900

    Added some activities and fragments

---
+++ app/src/main/java/com/bronto/homeworkmanager/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.bronto.homeworkmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
